37ba1ea2fe74f47f89adac4d31862389c7203387
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-21 21:24:08 +0600

    added a Nav Toggle and Nav animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@
 
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
-// import DaiayNav from './Components/DaisyNav/DaiayNav'
+import DaiayNav from './Components/DaisyNav/DaiayNav'
 
 function App() {
   
 
   return (
     <>
-    {/* <DaiayNav></DaiayNav> */}
     <NavBar></NavBar>
+    <DaiayNav></DaiayNav>
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
 
      
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,11 @@
+import { AiOutlineX } from "react-icons/ai";
+import { useState } from "react";
 import Link from "../Link/Link";
-
+import { AiOutlineAlignCenter } from "react-icons/ai";
 
 const NavBar = () => {
+   const [open, setOpen] = useState(false)
+   
     const routes = [
         { path: '/', name: 'Home', id: 1 },
         { path: '/about', name: 'About Us', id: 2},
@@ -12,6 +16,12 @@ const NavBar = () => {
    
     return (
         <nav>
+         <div className="md:hidden" onClick={()=> setOpen(!open)}>
+            {
+                open === true ? <AiOutlineX></AiOutlineX> :  <AiOutlineAlignCenter className="text-3xl " ></AiOutlineAlignCenter >
+            }
+        
+         </div>
           <ul className="md:flex">
           {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
